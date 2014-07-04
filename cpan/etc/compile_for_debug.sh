30cd39e41d5831fc1d95d7915563430adc2845ed
Jeffrey Kegler <JKEGL@cpan.org>
2014-07-03 22:29:05 -0700

    Clean out Marpa::R2 stuff

--- cpan/etc/compile_for_debug.sh
+++
@@ -1,3 +0,0 @@
-set -x
-rm -r libmarpa_build
-LIBMARPA_CFLAGS=-g ./Build code --Marpa-debug=1 --config optimize=-g
--- cpan/etc/dovg.sh
+++
@@ -1,8 +0,0 @@
-# Intended to be run from the cpan directory
-(ls t/*.t; ls html/t/*.t) |
-while read t
-do
-  echo === $t ===;
-  echo $t | perl -Ilib -Iblib/arch \
-    -E 'my $t = <STDIN>; chomp $t; use Test::Valgrind (file => $t, extra_supps => ["etc/my_suppressions"]);' 
-done 2>&1 | tee dovg.log
