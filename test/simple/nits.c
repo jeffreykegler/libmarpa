4fe0b492395d49fda75a71220b4e9532fd9550d6
rns <ruslan.shvedov@gmail.com>
2015-02-20 12:29:01 +0200

    add file to test Libmarpa nits on a simple, but not trivial grammar

---
+++ test/simple/nits.c
@@ -0,0 +1,180 @@
+/*
+ * Copyright 2015 Jeffrey Kegler
+ * This file is part of Libmarpa.  Libmarpa is free software: you can
+ * redistribute it and/or modify it under the terms of the GNU Lesser
+ * General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * Libmarpa is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser
+ * General Public License along with Libmarpa.  If not, see
+ * http://www.gnu.org/licenses/.
+ */
+
+/* Tests of Libmarpa methods on trivial grammar that is not merely nulling */
+
+#include <stdio.h>
+#include "marpa.h"
+
+#include "marpa_test.h"
+
+static int
+warn (const char *s, Marpa_Grammar g)
+{
+  printf ("%s returned %d\n", s, marpa_g_error (g, NULL));
+}
+
+static int
+fail (const char *s, Marpa_Grammar g)
+{
+  warn (s, g);
+  exit (1);
+}
+
+Marpa_Symbol_ID S_top;
+Marpa_Symbol_ID S_A1;
+Marpa_Symbol_ID S_A2;
+Marpa_Symbol_ID S_B1;
+Marpa_Symbol_ID S_B2;
+Marpa_Symbol_ID S_C1;
+Marpa_Symbol_ID S_C2;
+
+/* Longest rule is <= 4 symbols */
+Marpa_Symbol_ID rhs[4];
+
+Marpa_Rule_ID R_top_1;
+Marpa_Rule_ID R_top_2;
+Marpa_Rule_ID R_C2_3; // highest rule id
+
+/* For (error) messages */
+char msgbuf[80];
+
+char *
+symbol_name (Marpa_Symbol_ID id)
+{
+  if (id == S_top) return "top";
+  if (id == S_A1) return "A1";
+  if (id == S_A2) return "A2";
+  if (id == S_B1) return "B1";
+  if (id == S_B2) return "B2";
+  if (id == S_C1) return "C1";
+  if (id == S_C2) return "C2";
+  sprintf (msgbuf, "no such symbol: %d", id);
+  return msgbuf;
+}
+
+static Marpa_Grammar
+marpa_g_simple_new(Marpa_Config *config)
+{
+  Marpa_Grammar g;
+  g = marpa_g_new (config);
+  if (!g)
+    {
+      Marpa_Error_Code errcode = marpa_c_error (config, NULL);
+      printf ("marpa_g_new returned %d", errcode);
+      exit (1);
+    }
+
+  ((S_top = marpa_g_symbol_new (g)) >= 0)
+    || fail ("marpa_g_symbol_new", g);
+  ((S_A1 = marpa_g_symbol_new (g)) >= 0)
+    || fail ("marpa_g_symbol_new", g);
+  ((S_A2 = marpa_g_symbol_new (g)) >= 0)
+    || fail ("marpa_g_symbol_new", g);
+  ((S_B1 = marpa_g_symbol_new (g)) >= 0)
+    || fail ("marpa_g_symbol_new", g);
+  ((S_B2 = marpa_g_symbol_new (g)) >= 0)
+    || fail ("marpa_g_symbol_new", g);
+  ((S_C1 = marpa_g_symbol_new (g)) >= 0)
+    || fail ("marpa_g_symbol_new", g);
+  ((S_C2 = marpa_g_symbol_new (g)) >= 0)
+    || fail ("marpa_g_symbol_new", g);
+
+  rhs[0] = S_A1;
+  ((R_top_1 = marpa_g_rule_new (g, S_top, rhs, 1)) >= 0)
+    || fail ("marpa_g_rule_new", g);
+  rhs[0] = S_A2;
+  ((R_top_2 = marpa_g_rule_new (g, S_top, rhs, 1)) >= 0)
+    || fail ("marpa_g_rule_new", g);
+  rhs[0] = S_B1;
+  (marpa_g_rule_new (g, S_A1, rhs, 1) >= 0)
+    || fail ("marpa_g_rule_new", g);
+  rhs[0] = S_B2;
+  (marpa_g_rule_new (g, S_A2, rhs, 1) >= 0)
+    || fail ("marpa_g_rule_new", g);
+  rhs[0] = S_C1;
+  (marpa_g_rule_new (g, S_B1, rhs, 1) >= 0)
+    || fail ("marpa_g_rule_new", g);
+  rhs[0] = S_C2;
+  (marpa_g_rule_new (g, S_B2, rhs, 1) >= 0)
+    || fail ("marpa_g_rule_new", g);
+
+  return g;
+}
+
+static Marpa_Error_Code
+marpa_g_simple_precompute(Marpa_Grammar g, Marpa_Symbol_ID S_start)
+{
+  Marpa_Error_Code rc;
+
+  (marpa_g_start_symbol_set (g, S_start) >= 0)
+    || fail ("marpa_g_start_symbol_set", g);
+
+  rc = marpa_g_precompute (g);
+  if (rc < 0)
+    fail("marpa_g_precompute", g);
+
+  return rc;
+}
+
+int
+main (int argc, char *argv[])
+{
+  int rc;
+
+  Marpa_Config marpa_configuration;
+
+  Marpa_Grammar g;
+  Marpa_Recognizer r;
+
+  plan_lazy();
+
+  marpa_c_init (&marpa_configuration);
+  g = marpa_g_simple_new(&marpa_configuration);
+
+  /* for marpa_g_error() in marpa_m_test() */
+  marpa_m_grammar_set(g);
+
+  marpa_g_simple_precompute(g, S_top);
+  ok(1, "precomputation succeeded");
+
+  r = marpa_r_new (g);
+  if (!r)
+    fail("marpa_r_new", g);
+
+  diag ("The below test is before marpa_r_start_input()");
+  Marpa_Symbol_ID S_token = S_A2;
+  marpa_m_test("marpa_r_alternative", r, S_token, 0, 0, MARPA_ERR_RECCE_NOT_ACCEPTING_INPUT);
+
+  rc = marpa_r_start_input (r);
+  if (!rc)
+    fail("marpa_r_start_input", g);
+
+  diag ("The below recce tests are at earleme 0");
+
+  Marpa_Symbol_ID S_expected = S_A2;
+  int value = 1;
+  marpa_m_test("marpa_r_expected_symbol_event_set", r, S_expected, value, value);
+
+  /* recognizer reading methods */
+  marpa_m_test("marpa_r_alternative", r, S_invalid, 0, 0, MARPA_ERR_INVALID_SYMBOL_ID);
+  marpa_m_test("marpa_r_alternative", r, S_no_such, 0, 0, MARPA_ERR_NO_SUCH_SYMBOL_ID);
+  marpa_m_test("marpa_r_alternative", r, S_token, 0, 0, MARPA_ERR_RECCE_NOT_ACCEPTING_INPUT);
+  marpa_m_test("marpa_r_earleme_complete", r, -2, MARPA_ERR_RECCE_NOT_ACCEPTING_INPUT);
+
+  return 0;
+}
