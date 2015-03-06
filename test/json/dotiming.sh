7d174cc24d0a5069dd6c96a6ff36a72565ea2ea7
rns <ruslan.shvedov@gmail.com>
2015-03-06 16:32:36 +0200

    remove spurious line

--- test/json/dotiming.sh
+++ test/json/dotiming.sh
@@ -13,7 +13,6 @@ do
 $TIME -o timings.out --append --format="XS0 %S %U %e" perl -MJSON::XS -MData::Dumper -E 'local $Data::Dumper::Indent = 0; $/=undef; say Data::Dumper::Dumper(JSON::XS::decode_json(<STDIN>))' < test.in
 $TIME -o timings.out --append --format="PP0 %S %U %e" perl -MJSON::PP -MData::Dumper -E 'local $Data::Dumper::Indent = 0; $/=undef; say Data::Dumper::Dumper(JSON::PP::decode_json(<STDIN>))' < test.in
 $TIME -o timings.out --append --format="$MARPA_JSON %S %U %e" ./$MARPA_JSON test.in
-$TIME -o timings.out --append --format="json_c_es_id.exe %S %U %e" ./json_c_es_id.exe test.in
 $TIME -o timings.out --append --format="XS3 %S %U %e" perl -MJSON::XS -MData::Dumper -E 'local $Data::Dumper::Indent = 3; $/=undef; say Data::Dumper::Dumper(JSON::XS::decode_json(<STDIN>))' < test.in
 $TIME -o timings.out --append --format="PP3 %S %U %e" perl -MJSON::PP -MData::Dumper -E 'local $Data::Dumper::Indent = 3; $/=undef; say Data::Dumper::Dumper(JSON::PP::decode_json(<STDIN>))' < test.in
 done >timing.log 2>&1
