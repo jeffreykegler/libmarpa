e752a766af2a49d3745046c77ebfa8e06c43eb9d
Jeffrey Kegler <jeffreykegler@protonmail.com>
2022-04-06 19:14:04 -0400

    Issue #92 -- Work in progress
    Remove redundant tests

--- test/simple/issue92a.c
+++ test/simple/issue92a.c
@@ -196,7 +196,7 @@ main (int argc, char *argv[])
   API_test_data defaults;
   API_test_data this_test;
 
-  plan(121);
+  plan(33);
 
   marpa_c_init (&marpa_configuration);
   g = marpa_g_trivial_new(&marpa_configuration);
@@ -215,20 +215,6 @@ main (int argc, char *argv[])
     if (!r)
       fail("marpa_r_new", g);
 
-    /* the recce hasn't been started yet */
-
-    API_STD_TEST0(defaults, -1, MARPA_ERR_RECCE_NOT_STARTED, marpa_r_current_earleme, r);
-    API_STD_TEST0(defaults, -2, MARPA_ERR_RECCE_NOT_STARTED, marpa_r_progress_report_reset, r);
-    API_STD_TEST1(defaults, -2, MARPA_ERR_RECCE_NOT_STARTED, marpa_r_progress_report_start, r, whatever);
-    API_STD_TEST0(defaults, -2, MARPA_ERR_RECCE_NOT_STARTED, marpa_r_progress_report_finish, r);
-
-    {
-      int set_id;
-      Marpa_Earley_Set_ID origin;
-      API_STD_TEST2(defaults, -2, MARPA_ERR_RECCE_NOT_STARTED,
-	marpa_r_progress_item, r, &set_id, &origin);
-    }
-
     /* start the recce */
     rc = marpa_r_start_input (r);
     if (!rc)
@@ -236,249 +222,14 @@ main (int argc, char *argv[])
 
     diag ("The below recce tests are at earleme 0");
 
-    { /* event loop -- just count events so far -- there must be no event except exhausted */
-      Marpa_Event event;
-      int exhausted_event_triggered = 0;
-      int spurious_events = 0;
-      int prediction_events = 0;
-      int completion_events = 0;
-      int event_ix;
-      const int event_count = marpa_g_event_count (g);
-
-      is_int(1, event_count, "event count at earleme 0 is %ld", (long) event_count);
-
-      for (event_ix = 0; event_ix < event_count; event_ix++)
-      {
-        int event_type = marpa_g_event (g, &event, event_ix);
-        if (event_type == MARPA_EVENT_SYMBOL_COMPLETED)
-          completion_events++;
-        else if (event_type == MARPA_EVENT_SYMBOL_PREDICTED)
-          prediction_events++;
-        else if (event_type == MARPA_EVENT_EXHAUSTED)
-          exhausted_event_triggered++;
-        else
-        {
-          printf ("spurious event type is %ld\n", (long) event_type);
-          spurious_events++;
-        }
-      }
-
-      is_int(0, spurious_events, "spurious events triggered: %ld", (long) spurious_events);
-      is_int(0, completion_events, "completion events triggered: %ld", (long) completion_events);
-      is_int(0, prediction_events, "completion events triggered: %ld", (long) prediction_events);
-      ok (exhausted_event_triggered, "exhausted event triggered");
-
-    } /* event loop */
-
-    /* recognizer reading methods */
-    Marpa_Symbol_ID S_token = S_A2;
-    this_test.msg = "not accepting input is checked before invalid symbol";
-    API_CODE_TEST3(this_test, MARPA_ERR_RECCE_NOT_ACCEPTING_INPUT,
-        marpa_r_alternative, r, S_invalid, 0, 0);
-
-    this_test.msg = "not accepting input is checked before no such symbol";
-    API_CODE_TEST3(this_test, MARPA_ERR_RECCE_NOT_ACCEPTING_INPUT,
-        marpa_r_alternative, r, S_no_such, 0, 0);
-
-    this_test.msg = "not accepting input";
-    API_CODE_TEST3(this_test, MARPA_ERR_RECCE_NOT_ACCEPTING_INPUT,
-        marpa_r_alternative, r, S_token, 0, 0);
-
     this_test = defaults;
-
-    API_STD_TEST0(defaults, -2, MARPA_ERR_RECCE_NOT_ACCEPTING_INPUT,
-	marpa_r_earleme_complete, r);
 
     this_test.msg = "at earleme 0";
     API_STD_TEST0(this_test, 1, MARPA_ERR_NONE, marpa_r_is_exhausted, r);
 
-    /* Location accessors */
-    {
-      /* the below 2 always succeed */
-      unsigned int current_earleme = 0;
-      API_STD_TEST0(defaults, current_earleme, MARPA_ERR_NONE, marpa_r_current_earleme, r);
-
-      unsigned int furthest_earleme = current_earleme;
-      API_STD_TEST0(defaults, furthest_earleme, MARPA_ERR_NONE, marpa_r_furthest_earleme, r);
-
-      API_STD_TEST0(defaults, furthest_earleme, MARPA_ERR_NONE, marpa_r_latest_earley_set, r);
-
-      API_STD_TEST1(defaults, current_earleme, MARPA_ERR_NONE,
-	marpa_r_earleme, r, current_earleme);
-
-      API_STD_TEST1(defaults, -1, MARPA_ERR_RECCE_NOT_ACCEPTING_INPUT,
-	marpa_r_earley_set_value, r, current_earleme);
-
-      /* marpa_r_earley_set_value_*() methods */
-      int taxicab = 1729;
-
-      int earley_set;
-
-      struct marpa_r_earley_set_value_test {
-        int earley_set;
-
-        int rv_marpa_r_earleme;
-        int rv_marpa_r_latest_earley_set_value_set;
-        int rv_marpa_r_earley_set_value;
-        int rv_marpa_r_latest_earley_set_values_set;
-
-        int   rv_marpa_r_earley_set_values;
-        int   int_p_value_rv_marpa_r_earley_set_values;
-        void* void_p_value_rv_marpa_r_earley_set_values;
-
-        Marpa_Error_Code errcode;
-      };
-      typedef struct marpa_r_earley_set_value_test Marpa_R_Earley_Set_Value_Test;
-
-      const Marpa_R_Earley_Set_Value_Test tests[] = {
-        { -1, -2, taxicab,      -2, 1, -2, taxicab, value2, MARPA_ERR_INVALID_LOCATION },
-        {  0,  0, taxicab, taxicab, 1,  1,      42, value2, MARPA_ERR_INVALID_LOCATION },
-        {  1, -2,      42,      -2, 1, -2,      42, value2, MARPA_ERR_NO_EARLEY_SET_AT_LOCATION },
-        {  2, -2,      42,      -2, 1, -2,      42, value2, MARPA_ERR_NO_EARLEY_SET_AT_LOCATION },
-      };
-
-      for (ix = 0; ix < sizeof(tests) / sizeof(Marpa_R_Earley_Set_Value_Test); ix++)
-        {
-          const Marpa_R_Earley_Set_Value_Test t = tests[ix];
-          diag("marpa_r_earley_set_value_*() methods, earley_set: %d", t.earley_set);
-
-          if (t.earley_set == -1 || t.earley_set == 1 || t.earley_set == 2) {
-	    API_STD_TEST1(defaults, t.rv_marpa_r_earleme, t.errcode,
-	      marpa_r_earleme, r, t.earley_set);
-          } else {
-	    API_STD_TEST1(defaults, t.rv_marpa_r_earleme, MARPA_ERR_NONE,
-	      marpa_r_earleme, r, t.earley_set);
-	  }
-
-	  API_STD_TEST1(defaults, t.rv_marpa_r_latest_earley_set_value_set, MARPA_ERR_NONE,
-	    marpa_r_latest_earley_set_value_set, r, t.rv_marpa_r_latest_earley_set_value_set);
-
-          if (t.earley_set == -1 || t.earley_set == 1 || t.earley_set == 2) {
-	    API_STD_TEST1(defaults, t.rv_marpa_r_earley_set_value, t.errcode,
-	      marpa_r_earley_set_value, r, t.earley_set);
-          } else {
-	    API_STD_TEST1(defaults, t.rv_marpa_r_earley_set_value, MARPA_ERR_NONE,
-	      marpa_r_earley_set_value, r, t.earley_set);
-	  }
-
-	  {
-	    API_STD_TEST2(defaults,
-	      t.rv_marpa_r_latest_earley_set_values_set,
-	      MARPA_ERR_NONE,
-	      marpa_r_latest_earley_set_values_set,
-	      r,
-	      42, value2);
-	  }
-
-	  {
-	    /* There is no c89 portable way to test arbitrary pointers.
-	     * With ifdef's we could cover 99.999% of cases, but for now
-	     * we do not bother.
-	     */
-	    void *orig_value2 = NULL;
-	    void *value2 = orig_value2;
-
-	    API_STD_TEST3 (defaults,
-			   t.rv_marpa_r_earley_set_values,
-			   t.errcode,
-			   marpa_r_earley_set_values,
-			   r, t.earley_set, (&int_value), &value2);
-	    is_int (t.int_p_value_rv_marpa_r_earley_set_values,
-		    int_value, "marpa_r_earley_set_values() int* value");
-
-	  }
-
-        }
-    } /* Location Accessors */
-
-    /* Other parse status methods */
-    {
-      int boolean = 0;
-      int threshold = 1;
-      API_STD_TEST1(defaults, threshold, MARPA_ERR_NONE,
-	marpa_r_earley_item_warning_threshold_set, r, threshold);
-
-      API_STD_TEST0(defaults, threshold, MARPA_ERR_NONE, marpa_r_earley_item_warning_threshold, r);
-    }
-
-    {
-      Marpa_Symbol_ID S_expected = S_C1;
-      int value = 1;
-      API_STD_TEST2(defaults, -2, MARPA_ERR_SYMBOL_IS_NULLING, marpa_r_expected_symbol_event_set, r, S_B1, value);
-
-      {
-	Marpa_Symbol_ID buffer[42];
-	API_STD_TEST1(defaults, 0, MARPA_ERR_NONE, marpa_r_terminals_expected, r, buffer);
-      }
-
-      API_STD_TEST1(defaults, 0, MARPA_ERR_NONE,
-	marpa_r_terminal_is_expected, r, S_C1);
-      API_STD_TEST1(defaults, -2, MARPA_ERR_INVALID_SYMBOL_ID,
-	marpa_r_terminal_is_expected, r, S_invalid);
-      API_STD_TEST1(defaults, -2, MARPA_ERR_NO_SUCH_SYMBOL_ID,
-	marpa_r_terminal_is_expected, r, S_no_such);
-
-    } /* Other parse status methods */
-
-    /* Progress reports */
-    {
-      API_STD_TEST0(defaults, -2, MARPA_ERR_PROGRESS_REPORT_NOT_STARTED,
-          marpa_r_progress_report_reset, r);
-
-      API_STD_TEST0(defaults, -2, MARPA_ERR_PROGRESS_REPORT_NOT_STARTED,
-          marpa_r_progress_report_finish, r);
-
-      {
-	int set_id;
-	Marpa_Earley_Set_ID origin;
-	API_STD_TEST2(defaults, -2, MARPA_ERR_PROGRESS_REPORT_NOT_STARTED,
-	  marpa_r_progress_item, r, &set_id, &origin);
-      }
-
-
-      /* start report at bad locations */
-      Marpa_Earley_Set_ID ys_id_negative = -1;
-      API_STD_TEST1(defaults, -2, MARPA_ERR_INVALID_LOCATION,
-          marpa_r_progress_report_start, r, ys_id_negative);
-
-      Marpa_Earley_Set_ID ys_id_not_existing = 1;
-      API_STD_TEST1(defaults, -2, MARPA_ERR_NO_EARLEY_SET_AT_LOCATION,
-          marpa_r_progress_report_start, r, ys_id_not_existing);
-
-      /* start report at earleme 0 */
-      Marpa_Earley_Set_ID earleme_0 = 0;
-      this_test.msg = "no items at earleme 0";
-      API_STD_TEST1(this_test, 0, MARPA_ERR_NONE,
-          marpa_r_progress_report_start, r, earleme_0);
-
-      {
-	int set_id;
-	Marpa_Earley_Set_ID origin;
-	API_STD_TEST2(defaults, -1, MARPA_ERR_PROGRESS_REPORT_EXHAUSTED,
-	  marpa_r_progress_item, r, &set_id, &origin);
-      }
-
-
-      int non_negative_value = 1;
-      API_STD_TEST0(this_test, non_negative_value, MARPA_ERR_NONE,
-          marpa_r_progress_report_reset, r);
-
-      this_test.msg = "at earleme 0";
-      API_STD_TEST0(this_test, non_negative_value, MARPA_ERR_NONE,
-          marpa_r_progress_report_finish, r);
-    }
-
     /* Bocage, Order, Tree, Value */
     {
       /* Bocage */
-      Marpa_Earley_Set_ID ys_invalid = -2;
-      API_PTR_TEST1(defaults, MARPA_ERR_INVALID_LOCATION,
-          marpa_b_new, r, ys_invalid);
-
-      Marpa_Earley_Set_ID ys_non_existing = 1;
-      API_PTR_TEST1(defaults, MARPA_ERR_NO_PARSE,
-          marpa_b_new, r, ys_non_existing);
-
       Marpa_Earley_Set_ID ys_at_current_earleme = -1;
       Marpa_Bocage b = marpa_b_new(r, ys_at_current_earleme);
       if (!b)
